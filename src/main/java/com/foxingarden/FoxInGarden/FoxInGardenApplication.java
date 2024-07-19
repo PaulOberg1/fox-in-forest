77ff7752f3db79e6fa0a6e45d136e197b6bf5083
PaulOberg1 <pauloberg1931@gmail.com>
2024-07-19 17:05:58 +0100

    Initial project setup
    - Created basic project directory structure

---
+++ src/main/java/com/foxingarden/FoxInGarden/FoxInGardenApplication.java
@@ -0,0 +1,13 @@
+package com.foxingarden.FoxInGarden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxInGardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoxInGardenApplication.class, args);
+	}
+
+}
