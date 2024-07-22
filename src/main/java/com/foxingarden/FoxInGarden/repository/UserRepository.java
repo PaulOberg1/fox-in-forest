be05680791d0a3bc144794f8bb93f67b26de0246
PaulOberg1 <pauloberg1931@gmail.com>
2024-07-22 11:32:21 +0100

    Feature: Create UserRepository class
    - Defined UserRepository class in repository directory
    - Defined custom accessor method findByUsernameAndPassword

---
+++ src/main/java/com/foxingarden/FoxInGarden/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.foxingarden.FoxInGarden.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.foxingarden.FoxInGarden.model.User;
+import java.util.List;
+
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    public List<User> findByUsernameAndPassword(String username, String password);
+
+}
