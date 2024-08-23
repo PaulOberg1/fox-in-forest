4f7d90e6dfcaa014c7e6d71d8fc214f1d1fde6e4
PaulOberg1 <pauloberg1931@gmail.com>
2024-08-23 17:28:00 +0100

    Feature: Add score functionality to Player class
    - Added new attribute score, initialised to 0 in constructor
    - Added method updateScore to increment score attribute

--- src/main/java/com/foxingarden/FoxInGarden/model/domain/Player.java
+++ src/main/java/com/foxingarden/FoxInGarden/model/domain/Player.java
@@ -9,13 +9,18 @@ public class Player {
     private String id;
     private Deck deck;
     private Card currentCardPlayed;
+    private int score;
 
     public Player(String id, Deck deck) {
         this.id = id;
         this.deck = deck;
         this.currentCardPlayed = null;
+        this.score=0;
     }
     public void playCard(String suit, int rank) {
         currentCardPlayed = deck.playCard(suit,rank);
+    }
+    public void updateScore() {
+        this.score++;
     }
 }
