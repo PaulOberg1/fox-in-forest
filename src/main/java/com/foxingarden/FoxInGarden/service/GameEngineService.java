61a5a635616a0ee23251157ef098ca801ed33a3d
PaulOberg1 <pauloberg1931@gmail.com>
2024-08-23 19:57:04 +0100

    Feature: Update GameEngineService.java
    - Defined addPlayer method, adding new player to the game given ID
    - Defined getOtherPlayer method, acting as wrapper for Game class' getOtherPlayer method

--- src/main/java/com/foxingarden/FoxInGarden/service/GameEngineService.java
+++ src/main/java/com/foxingarden/FoxInGarden/service/GameEngineService.java
@@ -3,9 +3,11 @@ package com.foxingarden.FoxInGarden.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.foxingarden.FoxInGarden.dto.AddPlayerMessage;
 import com.foxingarden.FoxInGarden.dto.DetermineWinnerMessage;
 import com.foxingarden.FoxInGarden.dto.PlayCardMessage;
 import com.foxingarden.FoxInGarden.model.domain.Game;
+import com.foxingarden.FoxInGarden.model.domain.Player;
 
 @Service
 public class GameEngineService { 
@@ -17,8 +19,6 @@ public class GameEngineService {
         return game.determineWinner(new DetermineWinnerMessage());
     }
 
-
-    
     public int playCard(PlayCardMessage playCardMessage) {
         String playerId = playCardMessage.getPlayerId();
         String suit = playCardMessage.getSuit();
@@ -26,5 +26,14 @@ public class GameEngineService {
 
         game.playCard(playerId, suit, rank);
         return game.getCentralDeck().size();
+    }
+
+    public AddPlayerMessage addPlayer(String playerId) {
+        Player newPlayer = game.addPlayer(playerId);
+        return new AddPlayerMessage(newPlayer.getDeck());
+    }
+
+    public String getOtherPlayer(String playerId) {
+        return game.getOtherPlayerId(playerId);
     }
 };
