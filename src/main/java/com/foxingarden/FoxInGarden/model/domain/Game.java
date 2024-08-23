5763b907b0776c4e2f260f5d6a73fee263ba095a
PaulOberg1 <pauloberg1931@gmail.com>
2024-08-23 19:49:01 +0100

    Feature: Update Game domain class
    - Initialise class with totalDeck attribute
    - Defined generateTotalDeck method, returning a deck containing all possible cards in game
    - Removed drawDeck parameter from constructor, instead defining totalDeck value using generateTotalDeck method
    - Defined addPlayer method, to add a new player to the players list attribute
    - Defined extractRandomDeck method, extracting 11 random cards from the current deck, intended to be assigned to a player later
    - Defined getOtherPlayer method, locating player by given ID and returning ID of the other player in the game

--- src/main/java/com/foxingarden/FoxInGarden/model/domain/Game.java
+++ src/main/java/com/foxingarden/FoxInGarden/model/domain/Game.java
@@ -2,6 +2,8 @@ package com.foxingarden.FoxInGarden.model.domain;
 
 import java.util.List;
 import java.util.Map;
+import java.util.ArrayList;
+
 
 import com.foxingarden.FoxInGarden.dto.DetermineWinnerMessage;
 
@@ -9,26 +11,44 @@ import lombok.Getter;
 
 @Getter
 public class Game {
-    private Deck drawDeck;
+    private Deck totalDeck;
     private List<Player> players;
     private int playerTurn;
     private Card decreeCard;
     private Map<String,Card> centralDeck;
     private Map<String,Player> idToPlayerMap;
 
-    public Game (Deck drawDeck, List<Player> players, int playerTurn) {
-        this.drawDeck = drawDeck;
+    public Game (List<Player> players, int playerTurn) {
+        this.totalDeck = generateTotalDeck();
         this.players = players;
         this.playerTurn = playerTurn;
-        this.decreeCard = drawDeck.getCard(0);
 
         for (Player player : players) {
             idToPlayerMap.put(player.getId(),player);
         }
     }
 
+    public Deck generateTotalDeck() {
+        Deck curDeck = new Deck(new ArrayList<>());
+        String[] suits = {"bell","key","moon"};
+        for (String suit : suits) {
+            for (int rank=1; rank<12; rank++) {
+                curDeck.addCard(new Card(suit,rank));
+            }
+        }
+        return curDeck;
+    }
+
     public Player getPlayerById(String playerId) {
         return idToPlayerMap.get(playerId);
+    }
+
+    public Player addPlayer(String playerId) {
+        Deck playerDeck = extractRandomDeck();
+        Player player = new Player(playerId,playerDeck);
+        players.add(player);
+        idToPlayerMap.put(playerId, player);
+        return player;
     }
 
     public DetermineWinnerMessage determineWinner(DetermineWinnerMessage determineWinnerMessage) {
@@ -46,9 +66,7 @@ public class Game {
             determineWinnerMessage.setWinnerId(determineWinnerMessage.getPlayer2Id());
 
         return determineWinnerMessage;
-        
     }
-
 
     public void playCard(String playerId, String suit, int rank) {
         Player player = idToPlayerMap.get(playerId);
@@ -56,5 +74,16 @@ public class Game {
 
         centralDeck.put(playerId, player.getCurrentCardPlayed());
     }
+
+    public Deck extractRandomDeck() {
+        return totalDeck.extractRandomCards(11);
+    }
+
+    public String getOtherPlayer(String playerId) {
+        if (playerId == players.get(0).getId()) 
+            return players.get(1).getId();
+        return players.get(0).getId();
+    }
+
 
 }
