2bb8123bd1d8ce7042385989051c750843b0eb04
PaulOberg1 <pauloberg1931@gmail.com>
2024-08-23 16:22:36 +0100

    Bugfix: Fix errors in DetermineWinnerMessage DTO
    - Made class attributes private for encapsulation
    - Added constructor for quick setting of attributes upon construction

--- src/main/java/com/foxingarden/FoxInGarden/dto/DetermineWinnerMessage.java
+++ src/main/java/com/foxingarden/FoxInGarden/dto/DetermineWinnerMessage.java
@@ -6,7 +6,13 @@ import lombok.Getter;
 @Setter
 @Getter
 public class DetermineWinnerMessage extends BaseDTO{
-    String winnerId;
-    int Player1Score;
-    int Player2Score;
+    private String winnerId;
+    private int Player1Score;
+    private int Player2Score;
+
+    public DetermineWinnerMessage(String winnerId, int Player1Score, int Player2Score) {
+        this.winnerId = winnerId;
+        this.Player1Score = Player1Score;
+        this.Player2Score = Player2Score;
+    }
 }
