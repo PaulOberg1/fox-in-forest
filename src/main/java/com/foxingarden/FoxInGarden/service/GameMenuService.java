35ae272061a9677a84ee3cd983b14e0671a2da53
PaulOberg1 <pauloberg1931@gmail.com>
2024-08-23 19:58:37 +0100

    Feature: Define GameMenuService class
    - Defined GameMenuService class within new GameMenuService.java file
    - Defined createUser method, accessing ID of user entry in database given username and password
    - Defined createUser, accessing ID of new user entry made in database, created with given username and password

---
+++ src/main/java/com/foxingarden/FoxInGarden/service/GameMenuService.java
@@ -0,0 +1,29 @@
+package com.foxingarden.FoxInGarden.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.foxingarden.FoxInGarden.model.entity.User;
+import com.foxingarden.FoxInGarden.repository.UserRepository;
+
+@Service
+public class GameMenuService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private User user;
+
+    public long getUserId(String username, String password) {
+        this.user = userRepository.findByUsernameAndPassword(username,password).get(0);
+        return user.getId();
+    }
+
+    public long createUser(String username, String password) {
+        User user = new User(username,password);
+        userRepository.save(user);
+        return user.getId();
+    }
+
+
+}
