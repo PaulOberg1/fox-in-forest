f3d7a70c488273c4dc30836bb6bf502fb110da41
PaulOberg1 <pauloberg1931@gmail.com>
2024-08-23 16:17:25 +0100

    Feature: Add /playCard endpoint
    - Defined /playCard endpoint in GameEngineController
    - Make call to service layer and broadcast resulting update in player scores
    - If both cards played, determine winner and send P2P message to winning client

--- src/main/java/com/foxingarden/FoxInGarden/controller/GameEngineController.java
+++ src/main/java/com/foxingarden/FoxInGarden/controller/GameEngineController.java
@@ -3,12 +3,20 @@ package com.foxingarden.FoxInGarden.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.foxingarden.FoxInGarden.dto.BaseDTO;
-
+import com.foxingarden.FoxInGarden.dto.DetermineWinnerMessage;
+import com.foxingarden.FoxInGarden.dto.PlayCardMessage;
+import com.foxingarden.FoxInGarden.model.domain.Deck;
 import com.foxingarden.FoxInGarden.service.GameEngineService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+
+import java.util.List;
 
 
 
@@ -27,5 +35,18 @@ class GameEngineController{
 
     public void privateUpdate(String userId, String path, BaseDTO dto) {
         messagingTemplate.convertAndSendToUser(userId, path, dto);
+    }
+
+    @MessageMapping("/playCard")
+    public void playCard(PlayCardMessage playCardMessage) {
+        Deck centralDeck = gameEngineService.playCard(playCardMessage); //playCardMessage contains user id
+        broadcastUpdate("/broadcast/updateCentralDeck", playCardMessage); //send Central Deck instead of playcardmsg
+        if (centralDeck.length() == 2) {
+            // Send central deck to service layer, return DTO containing updated scores
+            DetermineWinnerMessage determineWinnerMessage = gameEngineService.determineWinner(centralDeck);
+            broadcastUpdate("broadcast/determineWinnerUpdate", determineWinnerMessage);
+            String playerId = determineWinnerMessage.getWinnerId();
+            privateUpdate(playerId, "/p2p/selectWinnerUpdate", determineWinnerMessage);
+        }
     }
 }
