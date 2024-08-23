558c1642cf5449b4a51ec79d8b0ee5a00163dbaa
PaulOberg1 <pauloberg1931@gmail.com>
2024-08-23 17:26:36 +0100

    Bugfix: Modify DetermineWinnerMessage DTO
    - Added default constructor to DetermineWinnerMessage DTO, fixing error in GameEngineService.java
    - Added attributes Player1Id and Player2Id to fix errors in Game.java

--- src/main/java/com/foxingarden/FoxInGarden/dto/DetermineWinnerMessage.java
+++ src/main/java/com/foxingarden/FoxInGarden/dto/DetermineWinnerMessage.java
@@ -9,10 +9,21 @@ public class DetermineWinnerMessage extends BaseDTO{
     private String winnerId;
     private int Player1Score;
     private int Player2Score;
+    private String Player1Id;
+    private String Player2Id;
 
-    public DetermineWinnerMessage(String winnerId, int Player1Score, int Player2Score) {
+    public DetermineWinnerMessage(String winnerId, int Player1Score, int Player2Score, String Player1Id, String Player2Id) {
         this.winnerId = winnerId;
         this.Player1Score = Player1Score;
         this.Player2Score = Player2Score;
+        this.Player1Id = Player1Id;
+        this.Player2Id = Player2Id;
+    }
+    public DetermineWinnerMessage() {
+        this.winnerId = "";
+        this.Player1Score = -1;
+        this.Player2Score = -1;
+        this.Player1Id = "";
+        this.Player2Id = "";
     }
 }
