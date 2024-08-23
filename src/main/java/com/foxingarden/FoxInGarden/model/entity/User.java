f1e06fbc6715627c4dd273832753c8ee74d1b6be
PaulOberg1 <pauloberg1931@gmail.com>
2024-08-23 19:47:33 +0100

    Refactor: Use lombok library in User model
    - Remove manually defined accessor and mutator methods in User model class
    - Replace with automatically generated methods with Lombok

--- src/main/java/com/foxingarden/FoxInGarden/model/entity/User.java
+++ src/main/java/com/foxingarden/FoxInGarden/model/entity/User.java
@@ -7,8 +7,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
 
+import lombok.Setter;
+import lombok.Getter;
 
 @Entity
+@Setter
+@Getter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,17 +31,6 @@ public class User {
         this.password = password;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public String getUsername() {
-        return username;
-    }
-    public String getPassword() {
-        return password;
-    }
+    
 
 }
