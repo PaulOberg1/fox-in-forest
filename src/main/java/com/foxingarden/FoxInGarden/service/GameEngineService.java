7c8b7dfd86ff5a602adc24955a8150fadfefea4e
PaulOberg1 <pauloberg1931@gmail.com>
2024-08-24 20:26:45 +0100

    Refactor: Rename methods in GameEngineService
    - Renamed getOtherPlayerId to switchPlayerControlFrom, maintaining consistency in method nomenclature between layers
    - Ensured use of AddPlayerMessage DTO called updated constructor, and recorded client ID - player mapping in clientPlayerMappingService
    - Rename determineWinner method to getPlayerData

--- src/main/java/com/foxingarden/FoxInGarden/service/GameEngineService.java
+++ src/main/java/com/foxingarden/FoxInGarden/service/GameEngineService.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.foxingarden.FoxInGarden.dto.AddPlayerMessage;
-import com.foxingarden.FoxInGarden.dto.DetermineWinnerMessage;
+import com.foxingarden.FoxInGarden.dto.PlayerDataMessage;
 import com.foxingarden.FoxInGarden.dto.PlayCardMessage;
 import com.foxingarden.FoxInGarden.model.domain.Game;
 import com.foxingarden.FoxInGarden.model.domain.Player;
@@ -15,8 +15,13 @@ public class GameEngineService {
     @Autowired
     private Game game;
 
-    public DetermineWinnerMessage determineWinner() {
-        return game.determineWinner(new DetermineWinnerMessage());
+    @Autowired
+    private ClientPlayerMappingService clientPlayerMappingService;
+
+    public PlayerDataMessage getPlayerData(String clientId) {
+        Player player1 = game.getPlayers().get(0);
+        Player player2 = game.getPlayers().get(1);
+        return new PlayerDataMessage(clientId, player1.getScore(), player2.getScore(), player1.getId(), player2.getId());
     }
 
     public int playCard(PlayCardMessage playCardMessage) {
@@ -28,12 +33,14 @@ public class GameEngineService {
         return game.getCentralDeck().size();
     }
 
-    public AddPlayerMessage addPlayer(String playerId) {
-        Player newPlayer = game.addPlayer(playerId);
-        return new AddPlayerMessage(newPlayer.getDeck());
+    public AddPlayerMessage addPlayer(String clientId) {
+        Player player = game.addPlayer(clientId);
+        clientPlayerMappingService.registerClient(clientId, player);
+        int numPlayers = game.getPlayers().size();
+        return new AddPlayerMessage(clientId,player.getDeck(),numPlayers); 
     }
 
-    public String getOtherPlayerId(String playerId) {
-        return game.getOtherPlayerId(playerId);
+    public String switchPlayerControlFrom(String prevPlayerId) {
+        return game.switchPlayerControlFrom(prevPlayerId);
     }
 };
