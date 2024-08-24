fecffa3bc85691c71fc6a9f9e45bea41118da5f2
PaulOberg1 <pauloberg1931@gmail.com>
2024-08-24 11:35:24 +0100

    Feature: Add /connect endpoint in controller layer
    - Defined /connect endpoint in GameMenuController class
    - Generates a unique session ID corresponding to given client ID
    - Registers client ID - session ID mapping in the service layer

--- src/main/java/com/foxingarden/FoxInGarden/controller/GameMenuController.java
+++ src/main/java/com/foxingarden/FoxInGarden/controller/GameMenuController.java
@@ -1,14 +1,20 @@
 package com.foxingarden.FoxInGarden.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.foxingarden.FoxInGarden.dto.AuthenticateUserMessage;
 import com.foxingarden.FoxInGarden.dto.BaseDTO;
 import com.foxingarden.FoxInGarden.service.GameMenuService;
 
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpAttributesContextHolder;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 
@@ -42,5 +48,19 @@ class GameMenuController{
         String password = authenticateUserMessage.getPassword();
         long userId = gameMenuService.createUser(username,password);
         privateUpdate(String.valueOf(userId),"p2p/signupUpdate");
+    }
+
+    @MessageMapping("/connect")
+    public void connect(String payload) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(payload);
+            String clientId = jsonNode.get("clientId").asText();
+            String sessionId = SimpAttributesContextHolder.currentAttributes().getSessionId();
+            gameMenuService.registerClient(clientId, sessionId);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }   
     }
 }
