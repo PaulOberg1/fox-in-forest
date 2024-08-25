70f7d5df06962a35033a2507dd4a56283f9a842d
PaulOberg1 <pauloberg1931@gmail.com>
2024-08-25 13:27:00 +0100

    Feature: Store game sessions in GameEngineService
    - Add concurrent hashmap attribute to GameEngineService, ensuring thread-safety
    - Maps game IDs to corresponding game objects
    - Edited remaining methods to require game ID parameters, and fetch corresponding game object
    - Edited getPlayerData, addPlayer and switchPlayerControl to require BaseEngineMessage attributes

--- src/main/java/com/foxingarden/FoxInGarden/service/GameEngineService.java
+++ src/main/java/com/foxingarden/FoxInGarden/service/GameEngineService.java
@@ -1,46 +1,65 @@
 package com.foxingarden.FoxInGarden.service;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.foxingarden.FoxInGarden.dto.AddPlayerMessage;
-import com.foxingarden.FoxInGarden.dto.PlayerDataMessage;
-import com.foxingarden.FoxInGarden.dto.PlayCardMessage;
+import com.foxingarden.FoxInGarden.dto.game_engine_dtos.AddPlayerMessage;
+import com.foxingarden.FoxInGarden.dto.game_engine_dtos.BaseEngineMessage;
+import com.foxingarden.FoxInGarden.dto.game_engine_dtos.CentralDeckMessage;
+import com.foxingarden.FoxInGarden.dto.game_engine_dtos.PlayCardMessage;
+import com.foxingarden.FoxInGarden.dto.game_engine_dtos.PlayerDataMessage;
 import com.foxingarden.FoxInGarden.model.domain.Game;
 import com.foxingarden.FoxInGarden.model.domain.Player;
 
 @Service
 public class GameEngineService { 
     
-    @Autowired
-    private Game game;
+    private final ConcurrentHashMap<String,Game> sessions = new ConcurrentHashMap<>();
 
     @Autowired
     private ClientPlayerMappingService clientPlayerMappingService;
 
-    public PlayerDataMessage getPlayerData(String clientId) {
+    public PlayerDataMessage getPlayerData(BaseEngineMessage baseEngineMessage) {
+        String clientId = baseEngineMessage.getClientId();
+        String gameId = baseEngineMessage.getGameId();
+        Game game = sessions.get(gameId);
+
         Player player1 = game.getPlayers().get(0);
         Player player2 = game.getPlayers().get(1);
-        return new PlayerDataMessage(clientId, player1.getScore(), player2.getScore(), player1.getId(), player2.getId());
+        return new PlayerDataMessage(clientId, gameId, player1.getScore(), player2.getScore(), player1.getId(), player2.getId());
     }
 
-    public int playCard(PlayCardMessage playCardMessage) {
-        String playerId = playCardMessage.getClientId();
+    public CentralDeckMessage playCard(PlayCardMessage playCardMessage) {
+        String clientId = playCardMessage.getClientId();
+        String gameId = playCardMessage.getGameId();
         String suit = playCardMessage.getSuit();
         int rank = playCardMessage.getRank();
 
-        game.playCard(playerId, suit, rank);
-        return game.getCentralDeck().size();
+        Game game = sessions.get(gameId);
+
+        game.playCard(clientId, suit, rank);
+
+        return game.getCentralDeckState(clientId);
     }
 
-    public AddPlayerMessage addPlayer(String clientId) {
+    public AddPlayerMessage addPlayer(BaseEngineMessage baseEngineMessage) {
+        String clientId = baseEngineMessage.getClientId();
+        String gameId = baseEngineMessage.getGameId();
+        Game game = sessions.get(gameId);
+
         Player player = game.addPlayer(clientId);
         clientPlayerMappingService.registerClient(clientId, player);
         int numPlayers = game.getPlayers().size();
-        return new AddPlayerMessage(clientId,player.getDeck(),numPlayers); 
+        return new AddPlayerMessage(clientId,gameId,player.getDeck(),numPlayers); 
     }
 
-    public String switchPlayerControlFrom(String prevPlayerId) {
-        return game.switchPlayerControlFrom(prevPlayerId);
+    public String switchPlayerControl(BaseEngineMessage baseEngineMessage) {
+        String clientId = baseEngineMessage.getClientId();
+        String gameId = baseEngineMessage.getGameId();
+        Game game = sessions.get(gameId);
+        
+        return game.switchPlayerControlFrom(clientId);
     }
 };
