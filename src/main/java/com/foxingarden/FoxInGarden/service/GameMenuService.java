10e1344649eea4d0238d7daf4f65a57991949563
PaulOberg1 <pauloberg1931@gmail.com>
2024-08-25 10:48:46 +0100

    Refactor: Split code between service layers
    - Moved code managing user repository from GameMenuService class into UserService class
    - Prevents two definitions of getUserId function, ensuring clarity

--- src/main/java/com/foxingarden/FoxInGarden/service/GameMenuService.java
+++ src/main/java/com/foxingarden/FoxInGarden/service/GameMenuService.java
@@ -1,39 +1,16 @@
 package com.foxingarden.FoxInGarden.service;
 
-import java.util.concurrent.ConcurrentHashMap;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.foxingarden.FoxInGarden.model.entity.User;
-import com.foxingarden.FoxInGarden.repository.UserRepository;
-
 @Service
 public class GameMenuService {
-
-    @Autowired
-    private UserRepository userRepository;
 
     @Autowired
     ClientUserMappingService clientUserMappingService;
 
     @Autowired
     ClientSessionMappingService clientSessionMappingService;
-
-    private User user;
-
-    private final ConcurrentHashMap<String,String> clientIdToSessionId = new ConcurrentHashMap<>();
-
-    public long getUserId(String username, String password) {
-        this.user = userRepository.findByUsernameAndPassword(username,password).get(0);
-        return user.getId();
-    }
-
-    public long createUser(String username, String password) {
-        User user = new User(username,password);
-        userRepository.save(user);
-        return user.getId();
-    }
 
     public String getSessionId(String clientId) {
         return clientSessionMappingService.getSessionId(clientId);
