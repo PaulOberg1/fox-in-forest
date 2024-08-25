8915638511ce65932060bf9bfd86dca1821cae5f
PaulOberg1 <pauloberg1931@gmail.com>
2024-08-25 15:41:18 +0100

    Feature: Moved methods out of Game.java
    - Moved getPlayerById and addPlayer from Game class to GameSession class
    - Removed redundant constructor parameters and made centralCards hash map concurrent to ensure thread safety
    - Removed redundant attributes including idToPlayerMap, as relevant functionality now implemented in GameSession class

--- src/main/java/com/foxingarden/FoxInGarden/model/domain/Game.java
+++ src/main/java/com/foxingarden/FoxInGarden/model/domain/Game.java
@@ -2,9 +2,12 @@ package com.foxingarden.FoxInGarden.model.domain;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import com.foxingarden.FoxInGarden.dto.game_engine_dtos.CentralDeckMessage;
+import com.foxingarden.FoxInGarden.dto.game_engine_dtos.PlayCardMessage;
 
+import java.security.KeyStore.Entry;
 import java.util.ArrayList;
 
 import lombok.Getter;
@@ -13,36 +16,21 @@ import lombok.Getter;
 public class Game {
     private String id;
     private Deck totalDeck;
-    private List<Player> players;
-    private int playerTurn;
-    private Card decreeCard;
-    private Map<String,Card> centralDeck;
-    private Map<String,Player> idToPlayerMap;
+    private ConcurrentHashMap<String,Card> centralCards = new ConcurrentHashMap<>();
 
-    public Game (String id, List<Player> players, int playerTurn) {
+    public Game (String id) {
         this.id = id;
         this.totalDeck = generateTotalDeck();
-        this.players = players;
-        this.playerTurn = playerTurn;
-
-        for (Player player : players) {
-            idToPlayerMap.put(player.getId(),player);
-        }
     }
 
-    public CentralDeckMessage getCentralDeckState(String clientId) {
-        ArrayList<String> playerIds = new ArrayList<>();
-        ArrayList<String> cardSuits = new ArrayList<>();
-        ArrayList<Integer> cardRanks = new ArrayList<>();
+    public Player addPlayer(String playerId) {
+        Deck playerDeck = extractRandomDeck();
+        Player player = new Player(playerId,playerDeck);
+        return player;
+    }
 
-        for (Player player : players) {
-            playerIds.add(player.getId());
-            
-            Card card = centralDeck.get(player.getId());
-            cardSuits.add(card.getSuit());
-            cardRanks.add(card.getRank());
-        }
-       return new CentralDeckMessage(clientId, id, playerIds, cardSuits, cardRanks);
+    public Deck extractRandomDeck() {
+        return totalDeck.extractRandomCards(11);
     }
 
     public Deck generateTotalDeck() {
@@ -56,34 +44,27 @@ public class Game {
         return curDeck;
     }
 
-    public Player getPlayerById(String playerId) {
-        return idToPlayerMap.get(playerId);
-    }
-
-    public Player addPlayer(String playerId) {
-        Deck playerDeck = extractRandomDeck();
-        Player player = new Player(playerId,playerDeck);
-        players.add(player);
-        idToPlayerMap.put(playerId, player);
-        return player;
-    }
-
-    public void playCard(String playerId, String suit, int rank) {
-        Player player = idToPlayerMap.get(playerId);
+    public void playCard(Player player, String suit, int rank) {
         player.playCard(suit, rank);
-
-        centralDeck.put(playerId, player.getCurrentCardPlayed());
+        centralCards.put(player.getId(), new Card(suit, rank));
     }
 
-    public Deck extractRandomDeck() {
-        return totalDeck.extractRandomCards(11);
+    public CentralDeckMessage getCentralDeckState(String clientId) {
+        ArrayList<String> playerIds = new ArrayList<>();
+        ArrayList<String> cardSuits = new ArrayList<>();
+        ArrayList<Integer> cardRanks = new ArrayList<>();
+
+        for (Map.Entry<String,Card> entry : centralCards.entrySet()) {
+            String playerId = entry.getKey();
+            Card card = entry.getValue();
+            
+            playerIds.add(playerId);
+            cardSuits.add(card.getSuit());
+            cardRanks.add(card.getRank());
+        }
+        return new CentralDeckMessage(clientId, id, playerIds, cardSuits, cardRanks);
     }
 
-    public String switchPlayerControlFrom(String prevPlayerId) {
-        if (prevPlayerId == players.get(0).getId()) 
-            return players.get(1).getId();
-        return players.get(0).getId();
-    }
 
 
 }
