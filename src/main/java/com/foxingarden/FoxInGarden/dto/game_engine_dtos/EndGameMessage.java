7572890f5d2e13521fb9681f1cc98a732a89f9a1
PaulOberg1 <pauloberg1931@gmail.com>
2024-08-25 16:53:01 +0100

    Feature: Define new EndGameMessage DTO class
    - Defined EndGameMessage DTO class to transfer player points across layers and to frontend
    - Stores player IDs and corresponding points

---
+++ src/main/java/com/foxingarden/FoxInGarden/dto/game_engine_dtos/EndGameMessage.java
@@ -0,0 +1,21 @@
+package com.foxingarden.FoxInGarden.dto.game_engine_dtos;
+
+import lombok.Getter;
+
+@Getter
+public class EndGameMessage extends BaseEngineMessage {
+    private String player1Id;
+    private String player2Id;
+    
+    private int player1Points;
+    private int player2Points;
+
+    public EndGameMessage(String clientId, String gameId, String player1Id, String player2Id, int player1Points, int player2Points) {
+        super(clientId, gameId);
+        this.player1Id = player1Id;
+        this.player2Id = player2Id;
+
+        this.player1Points = player1Points;
+        this.player1Points = player1Points;
+    }
+}
