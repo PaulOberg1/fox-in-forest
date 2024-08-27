c428c3f06446de79014e2a1f303fc81397455a9d
Robert Mascarenhas <robwmascarenhas@gmail.com>
2024-08-27 02:58:33 +0100

    ¯\_(ツ)_/¯

--- src/main/java/com/foxingarden/FoxInGarden/model/entity/User.java
+++ src/main/java/com/foxingarden/FoxInGarden/model/entity/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 
 import lombok.Setter;
@@ -13,6 +14,7 @@ import lombok.Getter;
 @Entity
 @Setter
 @Getter
+@Table(name = "app_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
