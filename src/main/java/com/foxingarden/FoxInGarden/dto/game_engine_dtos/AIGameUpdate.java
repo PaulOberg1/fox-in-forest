4f289c8b9bd8409f44fc784b898f78e1584cbd29
PaulOberg1 <pauloberg1931@gmail.com>
2024-09-01 10:05:32 +0100

    Bugfix: Add decree card to AIGameUpdate DTO
    - Added decreeCard attribute to AIGameUpdate DTO for display in the frontend

--- src/main/java/com/foxingarden/FoxInGarden/dto/game_engine_dtos/AIGameUpdate.java
+++ src/main/java/com/foxingarden/FoxInGarden/dto/game_engine_dtos/AIGameUpdate.java
@@ -1,5 +1,6 @@
 package com.foxingarden.FoxInGarden.dto.game_engine_dtos;
 
+import com.foxingarden.FoxInGarden.model.domain.Card;
 import com.foxingarden.FoxInGarden.model.domain.Deck;
 
 import lombok.Getter;
@@ -8,10 +9,12 @@ import lombok.Getter;
 public class AIGameUpdate extends BaseEngineMessage {
     Deck playerDeck;
     Deck opDeck;
+    Card decreeCard;
 
-    public AIGameUpdate(String clientId, String gameId, Deck playerDeck, Deck opDeck) {
+    public AIGameUpdate(String clientId, String gameId, Deck playerDeck, Deck opDeck, Card decreeCard) {
         super(clientId, gameId);
         this.playerDeck = playerDeck;
         this.opDeck = opDeck;
+        this.decreeCard = decreeCard;
     }
 }
