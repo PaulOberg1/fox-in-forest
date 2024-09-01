7d0e0ec2d6d6c49f1aee8c62519de776b94727a3
PaulOberg1 <pauloberg1931@gmail.com>
2024-09-01 10:00:02 +0100

    Feature: Add methods to Node class of MCTS
    - Added attributes isPlayerTurn, playerScore and opScore, zero-initialising the latter 2
    - Defined method getChildByAction to enable updating of root node of MCTS class
    - Defined method getBestAction for AI strategy component of MCTS class

--- src/main/java/com/foxingarden/FoxInGarden/mcts/Node.java
+++ src/main/java/com/foxingarden/FoxInGarden/mcts/Node.java
@@ -12,20 +12,32 @@ public class Node {
     private int totalReward;
     private State state;
     private Node parent;
+    private boolean isPlayerTurn;
     private HashMap<Action,Node> children;
     private ArrayList<Action> untriedActions;
+
+    private int playerScore;
+    private int opScore;
     
-    public Node(State state, Node parent) {
+    public Node(State state, Node parent, boolean isPlayerTurn) {
         this.state = state;
         this.parent = parent;
+        this.isPlayerTurn = isPlayerTurn;
         this.children = new HashMap<>();
         this.totalReward = 0;
         this.visits = 0;
         this.untriedActions = getLegalActions(state);
+
+        playerScore = 0;
+        opScore = 0;
     }
 
     public ArrayList<Action> getLegalActions(State state) {
-        return state.getLegalActions();
+        return state.getLegalActions(isPlayerTurn);
+    }
+
+    public Node getChildByAction(Action action) {
+        return children.get(action);
     }
 
     public Node selectChild() {
@@ -49,13 +61,34 @@ public class Node {
         return bestChild;
     }
 
+    public Action getBestAction() {
+        int maxChildReward = 0;
+        Action bestAction = null;
+
+        double logVisits = Math.log(visits);
+        
+        for (Entry<Action,Node> entry : children.entrySet()) {
+            Node child = entry.getValue();
+            double childReward = child.getTotalReward();
+            double childVisits = child.getVisits();
+
+            int childTotalReward = (int)((childReward/childVisits) + 2*Math.sqrt(logVisits/childVisits));
+
+            if (childTotalReward>maxChildReward) {
+                maxChildReward = childTotalReward;
+                bestAction = entry.getKey();
+            }
+        }
+        return bestAction;
+    }
+
     public boolean isFullyExpanded() {
         return untriedActions.size() != 0;
     }
 
     public void addChild(State state, Action action) {
         if (!children.containsKey(action)) {
-            Node child = new Node(state,this);
+            Node child = new Node(state,this,!isPlayerTurn);
             children.put(action,child);
         }
     }
